91088325648bf946f229b8adb71294ab58d4b189
onta <43575837+69slimm@users.noreply.github.com>
2022-07-27 12:01:49 +0700

    Update and rename regex.py to grep_word.py
    Add description on script and change name file

--- grep_word.py
+++ grep_word.py
@@ -1,11 +1,9 @@
 import re
 import sys
-# with open("sample.txt","r") as file:
-#     for line in file:
-#         if re.search("SMS", line):
-#             a = line.rstrip()
-#             print(a)
 
+# sample.txt is the file that we read
+# 'SMS' word is word that we looking for. So the looping will find every line in file that has SMS word
+# using rstrip() built-in function to remove extra enter in every line that it printed
 
 with open("sample.txt","r") as file:
     for line in file:
@@ -15,7 +13,3 @@ with open("sample.txt","r") as file:
             filename.write(a + "\n")
             filename.close
 
-
-# file_path = 'out.txt'
-# sys.stdout = open(file_path, "w")
-# print("anto")
