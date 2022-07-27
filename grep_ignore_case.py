632cd4e9737b0bcc052da72313041c8c4255b801
onta <43575837+69slimm@users.noreply.github.com>
2022-07-27 14:06:14 +0700

    add comment

--- grep_ignore_case.py
+++ grep_ignore_case.py
@@ -1,6 +1,10 @@
 import re
 import sys
 
+# This script to get a line sentence that has 'tsel' word that perform case-insensitive matching. 
+# So the line has words like 'Tsel' or 'tseL' ord 'tSel' etc, it will print the line. 
+# It has same function like bash command "grep -i" in linux
+
 with open("sample.txt","r") as file:
     for line in file:
         regex_name = re.compile(r'tsel', re.IGNORECASE)
